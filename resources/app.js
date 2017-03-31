f636934a6546d34d3f942df2fe3b325334b908ee
betping <betping1993@gmail.com>
2017-03-31 15:22:12 +0530

    Add files via upload

---
+++ resources/app.js
@@ -0,0 +1,13 @@
+var employee=
+   {
+     Name: "betping dak",
+     Age: 24,
+     Salary: 12000,
+	 Address:{
+				City:"Bangalore",
+			    State:"Karnataka",
+				PinCode:560036
+			 }
+   };
+   
+console.log(employee);
